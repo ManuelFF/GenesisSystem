d0cf435162fb5391ba8e96a67301557b37e11c7d
ManuelFF <manuel10_11@outlook.com>
2018-02-02 11:51:02 -0500

    nuevo Sistema de control creado -> se encargara de los codigos de inicio

--- GenesisSystem/src/ModuleWorker/SYSControl.java
+++ GenesisSystem/src/ModuleWorker/SYSControl.java
@@ -7,8 +7,10 @@ package ModuleWorker;
 
 import NCLPM.LOG;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import javax.swing.JFrame;
 
@@ -20,27 +22,70 @@ public class SYSControl
 {
     //fichero TMP
     private String appPath = System.getProperties().getProperty("user.home");
-    private File fichero = new File( appPath + "\\GenesisTemporalFile.tmp");
+    private File fichero = new File( appPath + "\\AppData\\Local\\Temp\\GenesisTemporalFile.tmp");
     
     SYSAUDIOCON sysau = new SYSAUDIOCON();
 
     JFrame jf=new JFrame();
     LOG lc = new LOG();
      
-    public boolean comprobar()
+    public int comprobar()
     {
-       jf.setAlwaysOnTop(true);
-       
-       if(fichero.exists())
-       {
-          // int codigo = leer();
-       }
-       
-       return false;
 
+        if(fichero.exists())
+       {
+          //SI EXISTE EL TMP
+          int codigo = read();
+          switch (codigo) 
+          {
+              case 1:
+                  //SISTEMA EN EJECUCION
+                  return codigo;
+              case 0:
+                  return codigo;
+              case 2:
+                  return codigo;
+              case 3:
+                  return codigo;
+              default:
+                  break;
+          }
+       }else
+           if(fichero.exists()==false)
+           {
+               //SI NO EXISTE EL TMP
+               makeTMP();
+               write("1");
+               return 3;
+           }
+       return 0;
     }
     
-    public int leer()
+    public void makeTMP()
+    {
+        try 
+        {
+           BufferedWriter writer = new BufferedWriter(new FileWriter( fichero ));
+           writer.close();
+
+        } catch (IOException e) 
+          {
+              System.err.println(e.getMessage());
+          }
+    }
+    
+    public void deleteTMP()
+    {
+        try 
+        {
+            fichero.delete();            
+        } catch (Exception e) 
+          {
+              
+          }
+    }
+    
+    public int read()
     {
         //otra formato
        String linea = "0";        
@@ -51,8 +96,9 @@ public class SYSControl
           bufferedReader = new BufferedReader( new FileReader( fichero ) );            
           while(bufferedReader.ready())
           {
-            linea = bufferedReader.readLine();            
+             linea = bufferedReader.readLine();
           }
+       bufferedReader.close();
             
        }catch (IOException e) 
         {
@@ -62,6 +108,43 @@ public class SYSControl
         return codigo;
     }  
 
+    public void write(String codigo)
+    {
+        try 
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fichero));
+            writer.write(codigo);
+            writer.close();
+            
+        } catch (Exception e) 
+          {
+              
+          }
+    }
+    
+    public void clear()
+    {
+        deleteTMP();
+        makeTMP();
+    }
+    
+    public void Close_System()
+    {
+        try 
+        {
+            if(fichero.exists())
+            {
+                fichero.delete();
+            }
+            
+            System.exit(0);
+            
+        } catch (Exception e) 
+        {
+          System.err.println(e.getMessage());
+        }
+        
+    }
     
     
     
