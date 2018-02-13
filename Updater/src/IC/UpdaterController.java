570ef651de94f575221f6587fa203b32247fdeac
ManuelFF <manuel10_11@outlook.com>
2018-02-13 12:45:49 -0500

    agregado metodo "abrir archivo"

--- Updater/src/IC/UpdaterController.java
+++ Updater/src/IC/UpdaterController.java
@@ -6,6 +6,11 @@
 package IC;
 
 import Core.DBCON;
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -21,18 +26,8 @@ public class UpdaterController
     private Connection cn;
     private ResultSet rs;
     private PreparedStatement st;
-    
-    public static void main(String[] args)
-    {
-        
-    }
-    
-    public UpdaterController()
-    {
-        System.out.println(Ver_SIS());
-        System.out.println("HELLO WORLD");
-    }
-    
+            
+
     //Obtener VER_SIS
     public String Ver_SIS()         
     {
@@ -48,7 +43,6 @@ public class UpdaterController
             {            
               sistema = rs.getString("VER_SIS");
             }
-            System.out.println(sistema);
             return sistema;
             
         }catch(SQLException sqlex)
@@ -67,8 +61,48 @@ public class UpdaterController
     }
     
     
+    //Cargar version de sistema en texto
+    public String cargar_version_texto()
+    {
+		
+	try 
+        {
+           String archivo = "ver.txt";
+           File f = new File(archivo);
+           // Permite abrir el archivo como SOLO LECTURA
+	   FileReader fr = new FileReader(f);
+           // El objeto br permite acceder al m�todo readLine() que 
+           // lee los datos del archivo l�nea por l�nea.
+           BufferedReader br = new BufferedReader(fr);
+           String linea;
+           while( (linea=br.readLine() )!= null )
+           {
+            //Pasamos los valores de la cadena linea al arreglo data[]
+            String data[] = linea.split("/");
+            //Separamos los datos en variables
+            String ver = data[0];
+            return ver;
+           }
+			
+	}catch (IOException e)
+         {
+           String des = "Error en el cargado de la versión de texto";
+           //lc.write(des,"SYSCON", e);
+         }	
+        return null;
+    }
     
-    
+    public void abrir_archivo(String archivo)
+    {
+       try 
+       {
+         File ObjectoFile = new File(archivo);
+         Desktop.getDesktop().open(ObjectoFile);
+       } catch (IOException ioex) 
+            {
+                //lc.write("Error al intentar abrir el archivo "+archivo, "MWCON", ioex);
+            }
+    }
     
     
 }
