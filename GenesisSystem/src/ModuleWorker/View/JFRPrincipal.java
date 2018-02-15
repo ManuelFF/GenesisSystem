84dc2cb0ef8b646e876c6d601b4262d7c91ee5e5
ManuelFF <manuel10_11@outlook.com>
2018-02-15 09:25:59 -0500

    cambiado el tiempo en el que se buscan las actualizaciones -Se ha aumentado el tiempo de busqueda de actualizaciones de 2 segundos a 200 segundos

--- GenesisSystem/src/ModuleWorker/View/JFRPrincipal.java
+++ GenesisSystem/src/ModuleWorker/View/JFRPrincipal.java
@@ -83,7 +83,7 @@ public class JFRPrincipal extends javax.swing.JFrame
         try 
         {
           SYSSCHEDULERCON SCheduler = new SYSSCHEDULERCON(); //SE ENCARGA DE LAS TAREAS PRE PROGRAMADAs
-          SCheduler.Execute_Scheduler(JOB_Update.class, 2);
+          SCheduler.Execute_Scheduler(JOB_Update.class, 200);
         } catch (SchedulerException SE) 
           {
             lc.write("Ha ocurrido un error al intentar encender el comprobador de actualizaciones", "JFRPrincipal", SE);
